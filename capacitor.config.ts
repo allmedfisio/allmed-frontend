5896c4172711188170b01fe39e5b8b9a6d49f619
Paolo Zuccaro <zuccaro.paolo@gmail.com>
2025-04-07 23:28:38 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'allmed-fisio-manager',
+  webDir: 'www'
+};
+
+export default config;
