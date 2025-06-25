280e602a073a1d960ab88238de9a41213fbc9163
Paolo Zuccaro <allmedfisio@gmail.com>
2025-06-25 17:58:48 +0200

    Trasformata in PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -9,6 +9,7 @@ import { TokenInterceptor } from './services/token.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AddPatientModalModule } from './add-patient-modal/add-patient-modal.module';
 import { AddDoctorModalModule } from './add-doctor-modal/add-doctor-modal.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,6 +21,12 @@ import { AddDoctorModalModule } from './add-doctor-modal/add-doctor-modal.module
     HttpClientModule,
     AddPatientModalModule,
     AddDoctorModalModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
